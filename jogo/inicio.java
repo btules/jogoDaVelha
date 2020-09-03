945ec04739f4c47dd707ac0293299e3757786dea
Barbara Adriane <barbaratules@gmail.com>
2020-09-03 07:44:28 -0300

    Jogo da velha

---
+++ jogo/inicio.java
@@ -0,0 +1,16 @@
+package jogo;
+
+import java.util.Scanner;
+
+public class inicio {
+
+	public static void main(String[] args) {
+		Scanner opcao = new Scanner(System.in);
+		String jogador;
+		
+		
+		jogador = opcao.next();
+		opcao.close();
+	}
+
+}
